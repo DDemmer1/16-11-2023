ca1b449addf940f2636e406526e28851635613a4
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-11-16 17:27:53 +0100

    initial commit

--- src/de/demmer/dennis/Wiederholung.java
+++ src/de/demmer/dennis/Wiederholung.java
@@ -1,5 +1,78 @@
 package de.demmer.dennis;
 
 public class Wiederholung {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		// - implizites casting - 
+		// 32bit
+		int i = 345;
+		
+		// 64bit
+		double d = i;
+		
+		
+		
+		// - explizites casting - 
+		// 64bit
+		double d1 = 600.11;
+		
+		// 32bit
+		int i1 = (int) d1;
+		
+		System.out.println(i1);
+		
+		
+		
+		//Szenario Textmessenger. Max. 40 Zeichen pro Nachricht
+		
+		String message = "Hallo, das ist meine erste Nachricht!";
+		
+		// if-else
+		
+		int messageLength = message.length();
+		
+		if(messageLength >= 40) {
+			System.out.println("Die Nachricht ist zu lang");
+		} else if(messageLength != 0){
+			System.out.println("Die Nachricht wurde versendet");
+		} else if(true) {
+			
+		} else {
+			
+		}
+		
+		
+		
+		int x = 10;
+		
+		//STRG + LEERTASTE -> switch-case-statement
+		switch (x) {
+		case 10:
+			System.out.println("Die variable hat den Wert 10");
+			break;
+		case 11:
+			System.out.println("Die var ist 11");
+			break;
+
+		default:
+			break;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
 
 }
