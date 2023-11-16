9cf93ed5a52850448b93a16613bd661a206f1993
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-11-16 16:58:30 +0100

    initial commit

---
+++ src/de/demmer/dennis/Hinweis.java
@@ -0,0 +1,31 @@
+package de.demmer.dennis;
+
+public class Hinweis {
+
+	
+static int checkInt(int toCheck) {
+		
+		//hier kommen jetzt die if blöcke hin
+	
+		//wenn toCheck größer als 0 ->  +1 zurück geben	
+		//wenn toCheck kleiner als 0 ->  -1 zurück geben
+
+	
+		
+		// ein Rückgabewert muss auf jedenfall ohne Bedingung erreichbar sein
+		// Wenn int toCheck nicht größer oder kleiner 0 ist, dann muss er zwangsläufig den Wert 0 haben
+		return 0;
+
+	}
+	
+
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println(checkInt(-23));
+	}
+
+}
